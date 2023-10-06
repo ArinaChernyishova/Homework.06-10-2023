a22043df3015848404415a93a10a4c98373c1546
Arina Chernyishova <chernyishova.aa@gmail.com>
2023-10-06 23:50:41 +0300

    Решение задачи 2

--- tasks.js
+++ tasks.js
@@ -1,10 +1,19 @@
-/** ------- Задача №
- *  ------- Тема:
- *  ------- Ссылка:
+/** ------- Задача № 2
+ *  ------- Тема: Объекты.
+ *  ------- Ссылка: https://learn.javascript.ru/task/is-empty
  *  ------- Условие задачи:
- */
-
-
-
+Напишите функцию isEmpty(obj), которая возвращает true, если у объекта нет свойств, иначе false.
+Должно работать так:
+    let schedule = {};
+    alert( isEmpty(schedule) ); // true
+    schedule["8:30"] = "get up";
+    alert( isEmpty(schedule) ); // false
+ */ 
 
 // ------- Решение:
+function isEmpty(obj) {
+    for (let key in obj) {
+        return false;
+    }
+    return true;
+}
